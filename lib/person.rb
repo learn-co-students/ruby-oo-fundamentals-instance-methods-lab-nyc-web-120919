698abbb403356415bd01e54f2b4a7647eb2205a5
github username <github email address>
2019-12-09 20:56:05 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+    def talk
+        puts "Hello World!"
+        def walk
+            puts "The Person is walking"
+        end 
+    end 
+end
+ada = Person.new
+ada.talk 
+ada.walk
